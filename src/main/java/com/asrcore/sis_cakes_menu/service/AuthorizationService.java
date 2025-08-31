042bfaeb490a56bea6f9f1c6e2cddfd167cf06be
yuriamaralSR <yuriamaral.yas@gmail.com>
2025-08-31 19:02:18 -0300

    feat(auth): create service class for auth

---
+++ src/main/java/com/asrcore/sis_cakes_menu/service/AuthorizationService.java
@@ -0,0 +1,20 @@
+package com.asrcore.sis_cakes_menu.service;
+
+import com.asrcore.sis_cakes_menu.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class AuthorizationService implements UserDetailsService {
+
+    private UserRepository userRepository;
+
+    public AuthorizationService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByLogin(username);
+    }
+}
