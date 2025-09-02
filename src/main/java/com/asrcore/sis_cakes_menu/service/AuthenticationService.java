b56e2a54bd79e158a22727961a415d5cbba6c830
yuriamaralSR <yuriamaral.yas@gmail.com>
2025-09-01 22:26:01 -0300

    refactor(auth): rename AuthorizationService to AuthenticationService

--- src/main/java/com/asrcore/sis_cakes_menu/service/AuthenticationService.java
+++ src/main/java/com/asrcore/sis_cakes_menu/service/AuthenticationService.java
@@ -7,11 +7,11 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
-public class AuthorizationService implements UserDetailsService {
+public class AuthenticationService implements UserDetailsService {
 
     private UserRepository userRepository;
 
-    public AuthorizationService(UserRepository userRepository) {
+    public AuthenticationService(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
