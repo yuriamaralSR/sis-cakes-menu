e42994ddf1c3510209520a77517b74bd8d84d8c6
Yuri Amaral <yuriamaral.yas@gmail.com>
2025-09-24 12:20:55 -0300

    test(auth): add database seeding for testing JWT configuration

---
+++ src/main/java/com/asrcore/sis_cakes_menu/infra/config/DevConfig.java
@@ -0,0 +1,27 @@
+package com.asrcore.sis_cakes_menu.infra.config;
+
+import com.asrcore.sis_cakes_menu.model.User;
+import com.asrcore.sis_cakes_menu.model.enums.UserRole;
+import com.asrcore.sis_cakes_menu.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@Profile("dev")
+public class DevConfig implements CommandLineRunner {
+
+    @Autowired
+    private UserRepository userRepository;
+    private BCryptPasswordEncoder passwordEncoder;
+
+    @Override
+    public void run(String... args) throws Exception {
+        String encryptedPassword = new BCryptPasswordEncoder().encode("admin");
+        User admin = new User("admin", "user admin", encryptedPassword, UserRole.ADMIN);
+
+        userRepository.save(admin);
+    }
+}
